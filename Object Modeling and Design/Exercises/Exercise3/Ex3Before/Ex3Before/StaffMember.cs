9230bccfdcf3d0914507805b2539d934f2220655
MercyyyPlzzz <43275203+MercyyyPlzzz@users.noreply.github.com>
2018-10-26 10:06:31 +0100

    Update

---
+++ Object Modeling and Design/Exercises/Exercise3/Ex3Before/Ex3Before/StaffMember.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3Before
+{
+    class StaffMember
+    {        
+        private string name;
+        private string address;
+        private string phone;
+        private string socialSecurityNumber;
+        private double payRate;
+        private double bonus;
+        private string staffMember;
+
+        public StaffMember(string name, string address, string phone, string socialSecNo, double bonus, double payRate)
+        {
+            this.name = name;
+            this.address = address;
+            this.phone = phone;
+            this.socialSecurityNumber = socialSecNo;
+            this.bonus = bonus;
+            this.payRate = payRate;
+            this.staffMember = "Executive";
+        }
+
+        public StaffMember(string name, string address, string phone)
+        {
+            this.name = name;
+            this.address = address;
+            this.phone = phone;
+            this.staffMember = "Volunteer";
+        }
+
+        public StaffMember(string name, string address, string phone, string socialSecNo, double payRate)
+        {
+            this.name = name;
+            this.address = address;
+            this.phone = phone;
+            this.socialSecurityNumber = socialSecNo;
+            this.payRate = payRate;
+            this.staffMember = "Employee";
+        }
+
+         public double pay()
+        {
+            if (this.getStaffMember() == "Employee")
+            {
+                return getPayRate();
+            }
+            else if (this.getStaffMember() == "Executive")
+            {
+                return this.getPayRate() + this.bonus;
+            }
+            else if(this.getStaffMember()=="Volunteer")
+            {
+                return this.getPayRate() + 0;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+  
+     public double getPayRate()
+        {
+            return this.payRate;
+        }
+
+        public string getStaffMember()
+        {
+            return this.staffMember;
+        }
+
+        public void Add()
+        {
+            try
+            {
+                // Add to DB code goes here //
+            }
+            catch (Exception ex)
+            {
+                System.IO.File.WriteAllText(@"c:\Error.txt", ex.ToString());
+            }
+        }
+
+
+    }
+}
